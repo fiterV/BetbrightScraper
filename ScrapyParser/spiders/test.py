f1d545ced8df08905fdfb44cb7c93c11e96f6558
Vadym Prokopets <vadymBlablalbal>
2017-01-13 01:05:27 +0200

    Code refactored phase 2

--- ScrapyParser/spiders/test.py
+++ ScrapyParser/spiders/test.py
@@ -1,19 +1,9 @@
 import scrapy
-from cssselect.parser import Selector
 from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
-import logging
-import re
 from ScrapyParser.items import ScrapyparserItem
-from termcolor import colored
-from selenium import webdriver
 from bs4 import BeautifulSoup
-
-#Fix the way you find the numbers array for competitors
-#because it's made in different ways on FRONT END PAGE ON THE SITE
-
-
-
+import re
 
 class MySpider(BaseSpider):
     name='betty'
@@ -22,10 +12,8 @@ class MySpider(BaseSpider):
 
     #combination of XPath and Beautiful soup makes life easier
     def parse_race(self, response):
-
         sel = Selector(response)
         racecards = (sel.xpath('//ul[@data-disable-event-container="#racecard_"]').extract())
-
         #easy trick(but slow) that let us avoid the recursion in our algorithm
         if len(racecards)>1:
             for i in range(0, len(racecards)-1):
@@ -42,7 +30,6 @@ class MySpider(BaseSpider):
             numbers = [c.getText() for c in horsesParentSoup.findChildren('div', {'class': 'cloth-number'})]
             odds = [c['id'][14:] for c in horsesParentSoup.findChildren('li', {'id': re.compile(r'previous_odds_[\d]+')})]
 
-
             participants = list(zip(participantNames, numbers, odds))
             participants = list(set(participants))
             participants = sorted(participants, key=lambda x: int(x[1]))
@@ -58,20 +45,8 @@ class MySpider(BaseSpider):
     def parse(self, response):
         sel = Selector(response)
         items = []
-
         x = sel.xpath('//table[@class="racing"]/tr/td/a[@class!="event_time "]/@href')
         for i in x:
             href = i.extract()
             if 'international' in href:
                 yield scrapy.Request(href, callback=self.parse_race)
-
-
-        # x = sel.xpath('//table[@class="racing"]/tr/td/a[@class!="event_time "]/text()')
-        # for s in x:
-        #     item = ScrapyparserItem()
-        #     item['name']=s.extract()
-        #     print(item)
-        #     items.append(item)
-        # #print('Answer goes right here')
-        # #print(x)
-        # return items
